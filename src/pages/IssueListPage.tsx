7dfde81e2f997e6629ca6def5d5786fa147da9f4
BHyeonKim <rlaqhguse@gmail.com>
2023-08-31 18:56:02 +0900

    feat: 무한 스크롤 구현

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,27 +1,47 @@
 import Advertisement from 'components/common/Advertisement'
 import IssueInfo from 'components/common/IssueInfo'
+import SpinnerBubble from 'components/common/SpinnerBubble'
 import { ITEM_PER_ADS } from 'constants/github'
-import { FC, Fragment } from 'react'
-import { StyledLink, Wrapper } from 'styles/GlobalStyles'
-import { Issues } from 'types/issue'
+import useIssues from 'hooks/useIssues'
+import { useEffect, useRef } from 'react'
+import { StyledLink } from 'styles/GlobalStyles'
+import { LastElment, List, ListItem } from 'styles/issue/issueListStyles'
 
-interface IssueListPageProps {
-  issues?: Issues
+const IssueListPage = () => {
+  const [issues, toNextPage, isLoading] = useIssues()
+  const lastElRef = useRef<HTMLLIElement>(null)
+  const containerRef = useRef<HTMLUListElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) toNextPage()
+      },
+      {
+        root: containerRef.current,
+      },
+    )
+
+    observer.observe(lastElRef.current!)
+
+    return () => observer.disconnect()
+  }, [toNextPage])
+
+  return (
+    <List ref={containerRef}>
+      {issues?.map((issue, index) => (
+        <ListItem key={issue.node_id}>
+          <StyledLink to={`${issue.number}`}>
+            <IssueInfo issue={issue} />
+          </StyledLink>
+          {(index + 1) % ITEM_PER_ADS === 0 && (
+            <Advertisement index={Math.ceil(index / ITEM_PER_ADS)} />
+          )}
+        </ListItem>
+      ))}
+      <LastElment ref={lastElRef}>{isLoading && <SpinnerBubble />}</LastElment>
+    </List>
+  )
 }
-
-const IssueListPage: FC<IssueListPageProps> = ({ issues }) => (
-  <Wrapper>
-    {issues?.map((issue, index) => (
-      <Fragment key={issue.node_id}>
-        <StyledLink to={`${issue.number}`}>
-          <IssueInfo issue={issue} />
-        </StyledLink>
-        {(index + 1) % ITEM_PER_ADS === 0 ? (
-          <Advertisement index={Math.ceil(index / ITEM_PER_ADS)} />
-        ) : null}
-      </Fragment>
-    ))}
-  </Wrapper>
-)
 
 export default IssueListPage
