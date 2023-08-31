95683bffcdaede466dcfea330ee4bc087897f967
HHB <bcl0206@gmail.com>
2023-08-31 18:39:39 +0900

    feat: issue 데이터 fetch api 연결

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -1,3 +1,30 @@
-const IssueDetailPage = () => <div>IssueDetailPage</div>
+import github from 'apis/github'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Wrapper } from 'styles/GlobalStyles'
+import { Issue } from 'types/issue'
 
+const IssueDetailPage = () => {
+  const [issue, setIssue] = useState<Issue>()
+  const { id } = useParams()
+
+  const fetchIssueData = async () => {
+    try {
+      const response = await github.getIssue(Number(id))
+      setIssue(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchIssueData()
+  }, [])
+
+  return (
+    <Wrapper>
+      <div>{issue && issue.id}</div>
+    </Wrapper>
+  )
+}
 export default IssueDetailPage
